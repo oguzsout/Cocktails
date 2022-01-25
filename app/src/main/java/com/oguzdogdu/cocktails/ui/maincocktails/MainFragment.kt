5d9c5de35f6eaeed34354239bf9f274060affdb2
oguzsout <oguzd494@gmail.com>
2022-01-25 23:24:25 +0300

    navigation setup

--- app/src/main/java/com/oguzdogdu/cocktails/ui/maincocktails/MainFragment.kt
+++ app/src/main/java/com/oguzdogdu/cocktails/ui/maincocktails/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.oguzdogdu.cocktails
+package com.oguzdogdu.cocktails.ui.maincocktails
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.oguzdogdu.cocktails.R
 
 class MainFragment : Fragment() {
     override fun onCreateView(
