f3dd9c65876f129bc4214ef6b08467f0946ff2c8
oguzsout <oguzd494@gmail.com>
2022-02-08 17:40:00 +0300

    searchability edited for CocktailFragment.kt

--- app/src/main/java/com/oguzdogdu/cocktails/ui/maincocktails/CocktailFragment.kt
+++ app/src/main/java/com/oguzdogdu/cocktails/ui/maincocktails/CocktailFragment.kt
@@ -2,10 +2,13 @@ package com.oguzdogdu.cocktails.ui.maincocktails
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.oguzdogdu.cocktails.base.BaseFragment
+import com.oguzdogdu.cocktails.data.model.Drink
 import com.oguzdogdu.cocktails.databinding.FragmentCocktailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +21,7 @@ class CocktailFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        searchViewSetup()
         observeData()
     }
 
@@ -26,18 +30,63 @@ class CocktailFragment :
             adapter = cocktailAdapter
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
+            hideProgressBar()
         }
+    }
+
+    private fun searchViewSetup() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(cocktailName: String?): Boolean {
+                cocktailName?.let {
+                    viewModel.getSearchCocktails(it)
+                    showProgressBar()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
     }
 
     private fun observeData() {
         lifecycle.coroutineScope.launchWhenCreated {
             viewModel.cocktailList.collect { cocktailState ->
-                cocktailState.data.also {
-                    if (it != null) {
-                        cocktailAdapter.drinks = it
+                when {
+                    cocktailState.isLoading -> showProgressBar()
+                    cocktailState.error.isNotEmpty() -> {
+                        hideProgressBar()
+                        Toast.makeText(requireContext(), cocktailState.error, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    cocktailState.data?.isNotEmpty() == true -> {
+                        hideProgressBar()
+                        cocktailAdapter.drinks = cocktailState.data
                     }
                 }
+//                if (cocktailState.isLoading) {
+//                    showProgressBar()
+//                }
+//                if (cocktailState.error.isNotBlank()) {
+//                    hideProgressBar()
+//                    Toast.makeText(requireContext(), cocktailState.error, Toast.LENGTH_SHORT).show()
+//                }
+//
+//                cocktailState.data?.let {
+//                    hideProgressBar()
+//                    cocktailAdapter.drinks = it
+//              }
             }
         }
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
     }
 }
